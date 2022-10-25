a905109dd520063ffc8fd8af8450b376b9d0c27e
Marton Nemeth <mn@zeroitlab.com>
2022-10-25 22:43:01 +0200

    Set up tabs as four spaces

--- lua/mn/options.lua
+++ lua/mn/options.lua
@@ -6,6 +6,11 @@ vim.g.python3_host_prog = "/usr/bin/python3"
 
 vim.cmd("colorscheme gruvbox") -- I haven't found a way to do this in Lua
 
+-- use spaces instead of tabs
+vim.o.expandtab = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 0 -- use the same value as tabstop
+
 -- Show diagnostics on hover, after `vim.o.updatetime` milliseconds
 vim.o.updatetime=300
 local group = vim.api.nvim_create_augroup("hover", {clear = true})
