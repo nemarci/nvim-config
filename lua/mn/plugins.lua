195d60e215240341ae49ca153ccbdc51e8229cd7
Marton Nemeth <mn@zeroitlab.com>
2023-07-14 13:49:15 +0200

    Configure bigfile

--- lua/mn/plugins.lua
+++ lua/mn/plugins.lua
@@ -163,19 +163,19 @@ require('lazy').setup({
   {
     'LunarVim/bigfile.nvim',
     config = function()
-        require("bigfile").config {
-          filesize = 10,     -- size of the file in MiB, the plugin round file sizes to the closest MiB
-          pattern = { "*" }, -- autocmd pattern
-          features = {       -- features to disable
-            "indent_blankline",
-            "lsp",
-            "treesitter",
-            "syntax",
-            "matchparen",
-            "vimopts",
-            "filetype",
-          },
-        }
+      require("bigfile").config {
+        filesize = 10,       -- size of the file in MiB, the plugin round file sizes to the closest MiB
+        pattern = { "*" },   -- autocmd pattern
+        features = {         -- features to disable
+          "indent_blankline",
+          "lsp",
+          "treesitter",
+          "syntax",
+          "matchparen",
+          "vimopts",
+          "filetype",
+        },
+      }
     end,
   },
 
