ea7434a0e1507049e9beb7263f114a4b958fca76
Marton Nemeth <mn@zeroitlab.com>
2024-09-12 11:28:24 +0200

    Added keymap to search errors only

--- lua/mn/telescope.lua
+++ lua/mn/telescope.lua
@@ -37,6 +37,10 @@ vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+vim.keymap.set('n', '<leader>se', function()
+  return require('telescope.builtin').diagnostics({ severity_limit = vim.diagnostic.severity.ERROR,
+    prompt_title = "Workspace errors" })
+end, { desc = '[S]earch [E]rrors' })
 vim.keymap.set('n', '<leader>fb', require('telescope').extensions.file_browser.file_browser,
   { desc = '[F]ile [B]rowser' })
 
